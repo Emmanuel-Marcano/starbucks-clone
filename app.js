5f286cee2ab8f5332c098e7de2c114fff9d2abea
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-03-22 17:32:29 +0100

    finally transioned the dropdown menu

--- app.js
+++ app.js
@@ -11,62 +11,71 @@ arrows.forEach(function(arrow){
     arrow.addEventListener("click", function(){
         if(arrow.parentElement.nextElementSibling.classList.contains("op-dropdown")){
 
-            if(op_dropdown.classList.contains("open")){
+            if(arrow.parentElement.parentElement.classList.contains("open")){
                 arrow.src = "images/down-arrow.png"
-                op_dropdown.classList.toggle("open")
-
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
             }
+
             else {
-                op_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
                 arrow.src = "images/swipe-up.png"
-            }  
+            }
         }
         else if(arrow.parentElement.nextElementSibling.classList.contains("bp-dropdown")){
             
-            if(bp_dropdown.classList.contains("open")){
+            if(arrow.parentElement.parentElement.classList.contains("open")){
                 arrow.src = "images/down-arrow.png"
-                bp_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
             }
 
             else {
-                bp_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
                 arrow.src = "images/swipe-up.png"
-
             }
         } 
         else if(arrow.parentElement.nextElementSibling.classList.contains("si-dropdown")){
             
-            if(si_dropdown.classList.contains("open")){
+            if(arrow.parentElement.parentElement.classList.contains("open")){
                 arrow.src = "images/down-arrow.png"
-                si_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
             }
 
             else {
-                si_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
                 arrow.src = "images/swipe-up.png"
             }
         }
         else if(arrow.parentElement.nextElementSibling.classList.contains("c-dropdown")){
             
-            if(c_dropdown.classList.contains("open")){
+            if(arrow.parentElement.parentElement.classList.contains("open")){
                 arrow.src = "images/down-arrow.png"
-                c_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
             }
 
             else {
-                c_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
                 arrow.src = "images/swipe-up.png"
             }
         }
         else if(arrow.parentElement.nextElementSibling.classList.contains("au-dropdown")){
             
-            if(au_dropdown.classList.contains("open")){
+            if(arrow.parentElement.parentElement.classList.contains("open")){
                 arrow.src = "images/down-arrow.png"
-                au_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
             }
 
             else {
-                au_dropdown.classList.toggle("open")
+                // au_dropdown.classList.toggle("open")
+                arrow.parentElement.parentElement.classList.toggle("open")
                 arrow.src = "images/swipe-up.png"
             }
         }
